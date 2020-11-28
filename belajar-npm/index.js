bafee988ef70d807817f9be07510e1623a03f013
jokojr97 <joko.riyadi97@gmail.com>
2020-11-28 07:58:11 +0700

    update belejar node html

---
+++ belajar-npm/index.js
@@ -0,0 +1,12 @@
+// import library http dari modul node_modul
+var http = require('http');
+// membuat server
+var server = http.createServer(function(req, res){
+    res.end("Hi, Selamat datang di Server");
+});
+
+// konfigurasi port server
+server.listen(8000);
+
+// menampilkan text tanda sedang menjalankan server
+console.log("server running on http://localhost:8000");
